7f6ce458e8b39d7b5c7c2b8ad0bb35fcdcb1cf45
Ganesh Jangir <ganeshnj@outlook.com>
2019-08-18 12:18:39 +0100

    Migrate database to expose foreign keys explicitly

--- src/LiveScore.Models/Match.cs
+++ src/LiveScore.Models/Match.cs
@@ -5,8 +5,11 @@ namespace LiveScore.Models
     {
         public int Id { get; set; }
 
-        public Team Team1 { get; set; }
-        public Team Team2 { get; set; }
+        public int? Team1Id { get; set; }
+        public virtual Team Team1 { get; set; }
+
+        public int? Team2Id { get; set; }
+        public virtual Team Team2 { get; set; }
 
         public int Team1Score { get; set; }
         public int Team2Score { get; set; }
