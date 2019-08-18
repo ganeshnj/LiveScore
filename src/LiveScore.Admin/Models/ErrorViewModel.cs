67d97a7b5de7af6daadbff70facdf96907bfc2e1
Ganesh Jangir <ganeshnj@outlook.com>
2019-08-18 10:37:00 +0100

    Add Admin project

---
+++ src/LiveScore.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LiveScore.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
